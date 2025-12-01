9adbc93aa282c9b52c269eb478d50085e60692a2
Hakan Kocadağ <hakankocadag038@gmail.com>
2025-12-01 17:20:26 +0300

    Add MergeSort implementation in MergeSort.java
    Implement Merge Sort algorithm for sorting an array.

---
+++ MergeSort.java
@@ -0,0 +1,54 @@
+package mergesort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    
+    public static void main(String[] args) {
+        int[] data = {5, 2, 8, 1, 9, 4, 7, 3, 6};
+        System.out.println("Original Array: " + Arrays.toString(data));
+
+        mergeSort(data);
+
+        System.out.println("Sorted Array:   " + Arrays.toString(data));
+    }
+   
+    public static void mergeSort(int[] list){
+    if(list.length > 1){
+    int[] firstHalf = new int[list.length/2];
+    System.arraycopy(list, 0, firstHalf, 0,list.length/2);
+    mergeSort(firstHalf);
+    
+    int secondHalfLength = list.length - list.length/2;
+    int[] secondHalf = new int [secondHalfLength];
+    System.arraycopy(list, list.length/2, secondHalf, 0, secondHalfLength);
+    mergeSort(secondHalf);
+    
+    merge(firstHalf,secondHalf,list);
+    } 
+  }
+  
+    public static void merge(int[] list1, int[] list2, int[] temp) {
+        
+    int current1 = 0;
+    int current2 = 0; 
+    int current3 = 0; 
+
+    while (current1 < list1.length && current2 < list2.length) {
+        if (list1[current1] < list2[current2]){
+            temp[current3++] = list1[current1++];
+        }
+        else{
+            temp[current3++] = list2[current2++];
+        }
+    }
+
+    while (current1 < list1.length){
+        temp[current3++] = list1[current1++];
+    }
+    while (current2 < list2.length){
+        temp[current3++] = list2[current2++];
+    }
+  }
+
+}
